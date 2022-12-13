d7dd980f8e58e0c482d1be75f2a2aabcc4a2ab1a
Edwin Tang <edwintang10@gmail.com>
2022-12-13 15:49:01 +0000

    feat(express): convert arena to express server

--- src/index.ts
+++ src/index.ts
@@ -7,9 +7,29 @@
  * Colyseus Server as documented here: 👉 https://docs.colyseus.io/server/api/#constructor-options 
  */
 import { listen } from "@colyseus/arena";
+import { WebSocketTransport } from "@colyseus/ws-transport";
+import { Server } from "colyseus";
+import { createServer } from "http";
+import express from "express";
 
 // Import arena config
 import arenaConfig from "./arena.config";
 
+
+const port = Number(process.env.port) || 2567;
+
 // Create and listen on 2567 (or PORT environment variable.)
-listen(arenaConfig);
+const app = express();
+app.use(express.json());
+
+const server = createServer(app);
+
+const gameServer = new Server({
+  transport: new WebSocketTransport({ server })
+});
+
+gameServer.listen(port);
+console.log(`Listening on ws://localhost:${port}`);
+
+
+//listen(arenaConfig);
